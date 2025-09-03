45a2d85804e7ce580b319781e5f58772316a5cc5
Glaudencio12 <glaudenciocosta015@gmail.com>
2025-09-02 21:34:23 -0300

    :recycle: refactor: Configura ambiente de desenvolvimento e produção

--- src/main/java/com/glaudencio12/Sistema_de_Controle_de_Despesas/config/DotEnvInitializer.java
+++ src/main/java/com/glaudencio12/Sistema_de_Controle_de_Despesas/config/DotEnvInitializer.java
@@ -7,8 +7,9 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class DotEnvInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().directory(System.getenv("DOTENV_PATH") != null ?
-                System.getenv("DOTENV_PATH") : ".").load();
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing()
+                .directory(System.getenv("DOTENV_PATH") != null ? System.getenv("DOTENV_PATH") : ".").load();
+
         System.setProperty("DB_URL_LOCAL", dotenv.get("DB_URL_LOCAL"));
         System.setProperty("DB_USER_LOCAL", dotenv.get("DB_USER_LOCAL"));
         System.setProperty("DB_PASSWORD_LOCAL", dotenv.get("DB_PASSWORD_LOCAL"));
